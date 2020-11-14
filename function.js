d6dfb2af84562b5804486cd5be7e7b7591f9c669
starbuckshiker <u1304954@umail.utah.edu>
2020-11-13 19:10:04 -0700

    odd!!

--- function.js
+++ function.js
@@ -19,20 +19,4 @@ alert(get_max(20,7,15));
         alert('Odd Number');
     }
 
-    import java.util.Scanner;
-
-public class EvenOdd {
-
-    public static void main(String[] args) {
-
-        Scanner reader = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int num = reader.nextInt();
-
-        if(num % 2 == 0)
-            System.out.println(num + " is even");
-        else
-            System.out.println(num + " is odd");
-    }
-}
+    
